91f7c6f32f1c1d11512edb63b19e386230574cb7
Pablo Niñoles Aznar <pnabty@gmail.com>
2014-03-12 15:42:21 +0100

    linked tornadoes at index.html

--- webgl/js/webgl.js
+++ webgl/js/webgl.js
@@ -41,12 +41,14 @@ function initBuffers() {
 
     squareVertexPositionBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
+
     vertices = [
          1.0,  1.0,  0.0,
         -1.0,  1.0,  0.0,
          1.0, -1.0,  0.0,
         -1.0, -1.0,  0.0
     ];
+    
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
     squareVertexPositionBuffer.itemSize = 3;
     squareVertexPositionBuffer.numItems = 4;
